5de0ff93646f133bd385d260d2724b25dbfa816f
gabsito <gacagonz@espol.edu.ec>
2022-11-11 22:25:05 -0500

    avance en proyecto 2

---
+++ Proyecto02/scripts.js
@@ -0,0 +1,44 @@
+//pedir datos de pokemon de pokeapi
+
+const container = document.querySelector(".container");
+function fetchPokemon(id) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      createPokemon(data);
+    });
+}
+
+function fetchPokemons(number) {
+    for (let i = 1; i <= number; i++) {
+        fetchPokemon(i);
+    }
+}
+
+function createPokemon(pokemon) {
+    const card = document.createElement("li");
+    card.classList.add("poke-block");
+
+    const spriteContainer = document.createElement("div");
+    spriteContainer.classList.add("img-container");
+
+    const sprite = document.createElement("img");
+    sprite.src = pokemon.sprites.front_default;
+
+    spriteContainer.appendChild(sprite);
+
+    const number = document.createElement("p");
+    number.textContent = `#${pokemon.id.toString().padStart(3, "0")}`;
+
+    const name = document.createElement("p");
+    name.classList.add("poke-name");
+    name.textContent = pokemon.name;
+
+    card.appendChild(spriteContainer);
+    card.appendChild(number);
+    card.appendChild(name);
+
+    container.appendChild(card);
+}
+
+fetchPokemons(9);
